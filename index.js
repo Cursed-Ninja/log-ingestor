bb5d0ae2ff796ec311cd6ec983a6a772357332c2
Shivam Mahajan <shivam.sm2002@gmail.com>
2023-11-17 22:57:44 +0530

    feat: initial setup

---
+++ index.js
@@ -0,0 +1,30 @@
+import dotenv from "dotenv";
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+// import cors from "cors";
+dotenv.config();
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+// app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello to Dyte API");
+});
+
+const CONNECTION_URL = `mongodb+srv://${
+  process.env.DB_USERNAME
+}:${encodeURIComponent(
+  process.env.DB_PSWD
+)}@cluster.lczmihh.mongodb.net/dyte?retryWrites=true&w=majority`;
+const PORT = process.env.PORT;
+
+mongoose
+  .connect(CONNECTION_URL)
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+  })
+  .catch((err) => console.log(err.message));
